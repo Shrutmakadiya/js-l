99bf2514f385e38caf6696bdc06ebdd875b8f861
Shrut27Makadiya <109706539+Shrutmakadiya@users.noreply.github.com>
2024-03-18 03:27:48 +0000

    add promise content

--- 06-Advance_One/Promise.js
+++ 06-Advance_One/Promise.js
@@ -21,12 +21,35 @@ new Promise(function(resolve, reject){
     console.log("Async 2 resolved");
 })
 
+//3
 const promiseThree = new Promise(function(resolve, reject){
     setTimeout(function(){
-        resolve({username: "Chai", email: "chai@example.com"})
+        resolve({username: "Makadiya ", email: "Makadiya @example.com"})
     }, 1000)
 })
 
 promiseThree.then(function(user){
     console.log(user);
 })
+
+//4
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = false
+        if (!error) {
+            resolve({username: "shrut", password: "123"})
+        } else {
+            reject('ERROR: Something went wrong')
+        }
+    }, 1000)
+})
+
+ promiseFour
+ .then((user) => {
+    console.log(user);
+    return user.username
+}).then((username) => {
+    console.log(username);
+}).catch(function(error){
+    console.log(error);
+}).finally(() => console.log("The promise is either resolved or rejected"))
