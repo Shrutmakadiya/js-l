e7c3c943c775d89e2b5c615e7d7859c2e286e109
Shrut27Makadiya <109706539+Shrutmakadiya@users.noreply.github.com>
2024-03-28 03:17:29 +0000

    add inheritnace and modern syntax

--- 07-Advance_oops/prototype.js
+++ 07-Advance_oops/prototype.js
@@ -28,3 +28,42 @@ Array.prototype.heyshrut = function(){
 // myHeros.shrut()
 // myHeros.heyshrut()
 // heroPower.heyshrut()
+
+
+
+// inheritance
+
+const User = {
+    name: "amtics",
+    email: "amtics@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__ = User
+
+// modern syntax
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+let anotherUsername = "amticsAurCode     "
+
+String.prototype.trueLength = function(){
+    console.log(`${this}`);
+    console.log(`True length is: ${this.trim().length}`);
+}
+
+anotherUsername.trueLength()
+"shrut".trueLength()
+"iceTea".trueLength()
