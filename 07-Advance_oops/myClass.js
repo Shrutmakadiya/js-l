1ffc8102dabbf9bb530439209d3cc46f182379ce
Shrut27Makadiya <109706539+Shrutmakadiya@users.noreply.github.com>
2024-03-28 15:14:08 +0000

    add class behaind

--- 07-Advance_oops/myClass.js
+++ 07-Advance_oops/myClass.js
@@ -20,3 +20,25 @@ const shrut = new User("shrut", "shrut@gmail.com", "123")
 
 console.log(shrut.encryptPassword());
 console.log(shrut.changeUsername());
+
+
+// behind the scene
+
+function User(username, email, password){
+    this.username = username;
+    this.email = email;
+    this.password = password
+}
+
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
+User.prototype.changeUsername = function(){
+    return `${this.username.toUpperCase()}`
+}
+
+
+const tea = new User("tea", "tea@gmail.com", "123")
+
+console.log(tea.encryptPassword());
+console.log(tea.changeUsername());
